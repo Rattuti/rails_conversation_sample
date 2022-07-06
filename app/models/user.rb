25bfcf489d56783050660fe25a33063ffec323a1
Rattuti <i.ishiharar.96296@gmail.com>
2022-07-06 11:44:03 +0900

    sample2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :conversations
+  has_many :messages, dependent: :destroy
+  
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
