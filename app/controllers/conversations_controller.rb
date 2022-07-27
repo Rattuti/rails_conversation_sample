22586ac14460fecfb37b747b7d91840442fe3bf7
Rattuti <i.ishiharar.96296@gmail.com>
2022-07-27 12:10:28 +0900

    second

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,18 +1,21 @@
 class ConversationsController < ApplicationController
     def index
-        @conversations = Conversation.with_user(current_user)
+      @conversations = Conversation.where(sender_id: current_user.id)
+      .or(Conversation.where(recipient_id: current_user.id))
     end
-
-  def create
-    unless @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).take
-      @conversation = Conversation.create!(conversation_params)
+  
+  
+    def create
+      unless @conversation = Conversation.where(sender_id: params[:sender_id], recipient_id: params[:recipient_id])
+      .or(Conversation.where(sender_id: params[:recipient_id], recipient_id: params[:sender_id])).take
+        @conversation = Conversation.create!(conversation_params)
+      end
+      redirect_to conversation_messages_path(@conversation)
     end
-    redirect_to conversation_messages_path(@conversation)
-  end
-    
+  
     private
-    
+  
     def conversation_params
-        params.permit(:sender_id, :recipient_id)
+      params.permit(:sender_id, :recipient_id)
     end
-end
+  end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,7 +4,14 @@ class Conversation < ActiveRecord::Base
     has_many :messages, dependent: :destroy
     validates_uniqueness_of :sender_id, scope: :recipient_id
   
-    scope :with_user, -> (current_user) {where(sender_id: current_user.id).or(Conversation.where(recipient_id: current_user.id))}
-  
-    scope :between, -> (sender_id, recipient_id) {where(sender_id: sender_id, recipient_id: recipient_id).or(Conversation.where(sender_id: recipient_id, recipient_id: sender_id))}
+    def target_user(current_user)
+      if sender_id == current_user.id
+        User.find(recipient_id)
+      elsif recipient_id == current_user.id
+        User.find(sender_id)
+      end
+    end
+
+    #scope :with_user, -> (current_user) {where(sender_id: current_user.id).or(Conversation.where(recipient_id: current_user.id))}
+    #scope :between, -> (sender_id, recipient_id) {where(sender_id: sender_id, recipient_id: recipient_id).or(Conversation.where(sender_id: recipient_id, recipient_id: sender_id))}
   end
